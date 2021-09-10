bc594ecd950aa23229df089d89e6daaf32a548ec
zhangping12 <634001184@qq.com>
2021-09-10 17:28:58 +0800

    开发Eureka server

---
+++ eureka-server/src/main/java/com/imooc/course/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.imooc.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 描述：Eureka的服务端
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
